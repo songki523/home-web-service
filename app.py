1567e8dba00a82a3fbe077e6a27c257d5c37e64d
Keith Song <songki523@gmail.com>
2020-12-07 05:12:20 +0000

    Created schduler

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
-from flask import Flask
+from flask import Flask, request, abort
+from flask import render_template, send_from_directory
+from csv_handler import CsvHandler
+import datetime, os
+
 app = Flask(__name__)
+app.config["CLIENT_CSV"] = os.path.expanduser("~/web-apps/flask/csv")
 
+_today = datetime.date.today()
+_year = _today.year
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        _collections = []
+        name = request.form['name']
+        month = request.form['month']
+        days = request.form['days']
+        
+        date_list = days.split(",")
+
+        year = is_month_january(month, _year)
+
+        csvhandler = CsvHandler("DayShift-" + str(_today))
+
+        for day in date_list:
+            _collections.append(
+                {"Subject": "{} Day Shift".format(name),
+                "Start Date": "{}/{}/{}".format(month, day, year)}
+            )
+        
+        csvhandler.store_into_csv(_collections)
+
+        try:
+            return send_from_directory(app.config["CLIENT_CSV"], filename=csvhandler.filename + ".csv", as_attachment=True)
+        except FileNotFoundError:
+            abort(404)
+
+    return render_template('index.html')
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
 
+# Generate Calendar to Next Year when a user enters next year
+def is_month_january(_month, _year):
+    ret = 0
+    if int(_month) == 1:
+        ret = int(_year) + 1
+    else:
+        ret = _year
+
+    return ret
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug='True')
---
+++ csv_handler.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+import csv
+
+class CsvHandler:
+    def __init__(self, filename):
+        self.filename = filename
+    
+    # Save it as csv file
+    def store_into_csv(self, collections):
+        relative_path = os.path.expanduser("~/web-apps/flask/csv")
+        _file_name = relative_path + "/{}.csv".format(self.filename)
+        file_exists = os.path.isfile(_file_name)
+
+        with open(_file_name, 'w') as _csvFile:
+            _fieldnames = ['Subject', 'Start Date']
+            writer = csv.DictWriter(_csvFile, fieldnames=_fieldnames)
+
+            if not file_exists:
+                writer.writeheader()
+            writer.writerows(collections)
+    
